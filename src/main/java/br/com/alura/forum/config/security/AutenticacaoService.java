77ed73d243bb9fdb77746935791afc18691633b4
ricardospinoza <ricardospinoza@gmail.com>
2022-11-21 08:38:54 -0300

    Add segurança por token jwt

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
@@ -1,6 +1,4 @@
-package br.com.alura.forum.security;
-
-import java.util.Optional;
+package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,7 +6,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.com.alura.forum.modelo.Usuario;
 import br.com.alura.forum.repository.UsuarioRepository;
 
 @Service
@@ -19,12 +16,12 @@ public class AutenticacaoService implements UserDetailsService{
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//return repository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Dados Invalidos"));
-		Optional<Usuario> usuario = repository.findByEmail(username);
-		if (usuario.isPresent()) {
-			return usuario.get();
-		}
-		throw new UsernameNotFoundException("Dados Invalidos");	
+		return repository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Dados Invalidos"));
+//		Optional<Usuario> usuario = repository.findByEmail(username);
+//		if (usuario.isPresent()) {
+//			return usuario.get();
+//		}
+//		throw new UsernameNotFoundException("Dados Invalidos");	
 	}
 
 }
